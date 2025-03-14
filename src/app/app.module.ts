a23627d9bae464ce0229f4b78ead18b43e0c49ea
Carvalh0Henrique <carvalhoghenrique@gmail.com>
2025-03-14 11:12:09 -0300

    Segunda versão

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { FormComponent } from './form/form.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { FormComponent } from './form/form.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NgbModule
+    NgbModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -7,5 +7,28 @@ import { Component } from '@angular/core';
   styleUrl: './form.component.css'
 })
 export class FormComponent {
+  num1: number = 0;
+  num2: number = 0;
+  resultado: number = 0;
+  text: string = 'Tipo de conta: ';
+  
+  soma1() {
+    this.resultado = this.num1 + this.num2;
+    this.text = 'Soma: '
+  }
 
-}
+  subt1() {
+    this.resultado = this.num1 - this.num2;
+    this.text = 'Subtração: '
+  }
+  
+  multi1() {
+    this.resultado = this.num1 * this.num2;
+    this.text = 'Multiplicação: '
+  }
+
+  divi1() {
+    this.resultado = this.num1 / this.num2;
+    this.text = 'Divisão: '
+  }
+} 
